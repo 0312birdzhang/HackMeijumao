54707678c49363193ab26a414be196a40b84c364
BirdZhang <0312birdzhang@gmail.com>
2016-06-20 17:24:39 +0800

    init

---
+++ exportMeijumao.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2016年6月20日
+
+@author: 0312birdzhang
+@summary: Hack for meijumao.cc
+'''
+import m3u8
+
+m3u8_obj = m3u8.load('http://ts.ystt.me/m3u8_Game_of_Thrones_S06E09-1.m3u8?pm3u8/0/expires/18000&e=1466426663&token=cPLJw_uAAGMXzHbzxCoqDtiTVD4LiTw6Oc19fxI2:NltqKTuWqRVo0TTdioXZdvBlmF0=')  # this could also be an absolute filename
+print m3u8_obj.dump("F:\\test\\test.playlist")
