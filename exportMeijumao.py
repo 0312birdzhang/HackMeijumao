72d23b553e8f7e4793a514db9ed266962ed3bb9b
鸟哥 <0312birdzhang@gmail.com>
2016-06-20 17:26:40 +0800

    Update exportMeijumao.py

--- exportMeijumao.py
+++ exportMeijumao.py
@@ -9,4 +9,4 @@ Created on 2016年6月20日
 import m3u8
 
 m3u8_obj = m3u8.load('http://ts.ystt.me/m3u8_Game_of_Thrones_S06E09-1.m3u8?pm3u8/0/expires/18000&e=1466426663&token=cPLJw_uAAGMXzHbzxCoqDtiTVD4LiTw6Oc19fxI2:NltqKTuWqRVo0TTdioXZdvBlmF0=')  # this could also be an absolute filename
-print m3u8_obj.dump("F:\\test\\test.playlist")
+m3u8_obj.dump("F:\\test\\test.playlist")
